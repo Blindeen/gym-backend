1aca4b8953774fa329bc99a728b969ac934810f5
Blindeen <kuba.szmajda@gmail.com>
2024-03-29 21:43:44 +0100

    move methods from authentication service to member service

--- src/main/java/project/gym/service/MemberService.java
+++ src/main/java/project/gym/service/MemberService.java
@@ -1,31 +1,63 @@
 package project.gym.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import project.gym.config.JwtService;
+import project.gym.dto.AuthenticationResponse;
+import project.gym.dto.LoginMemberDto;
 import project.gym.dto.RegisterMemberDto;
+import project.gym.exception.EmailAlreadyExistException;
 import project.gym.model.Contact;
 import project.gym.model.Member;
 import project.gym.repo.MemberRepo;
 
 @Service
-public class MemberService implements UserDetailsService {
+public class MemberService {
     @Autowired
     private MemberRepo memberRepo;
 
-    public void createMember(RegisterMemberDto registerMemberDto) {
-        Member newMember = registerMemberDto.toMember();
-        Contact newContact = registerMemberDto.toContact();
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterMemberDto request) {
+        Member newMember = request.toMember();
+        Contact newContact = request.toContact();
+
+        newMember.setPassword(passwordEncoder.encode(newMember.getPassword()));
         newMember.setContact(newContact);
-        memberRepo.save(newMember);
+
+        try {
+            memberRepo.save(newMember);
+        } catch (DataIntegrityViolationException e) {
+            throw new EmailAlreadyExistException();
+        }
+
+        String token = jwtService.generateToken(newMember);
+
+        return new AuthenticationResponse(token);
     }
 
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return memberRepo.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("Email not found"));
+    public AuthenticationResponse authentication(LoginMemberDto request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+        Member member = memberRepo.findByEmail(request.getEmail()).orElseThrow();
+        String token = jwtService.generateToken(member);
+
+        return new AuthenticationResponse(token);
     }
 }
