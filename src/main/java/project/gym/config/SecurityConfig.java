ce11dfc0d9d1c71e8c515d70268c69cc98a4b2cc
Blindeen <kuba.szmajda@gmail.com>
2024-05-26 18:13:07 +0200

    protect some activity routes

--- src/main/java/project/gym/config/SecurityConfig.java
+++ src/main/java/project/gym/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import project.gym.enums.Role;
 import project.gym.service.UserDetailsImplService;
 
 @Configuration
@@ -35,6 +36,12 @@ public class SecurityConfig {
                 .authorizeHttpRequests(
                         req -> req.requestMatchers("/api/member/login", "/api/member/register")
                                 .permitAll()
+                                .requestMatchers(
+                                        "/api/activity/create",
+                                        "/api/activity/{id}/update",
+                                        "/api/activity/{id}/delete"
+                                )
+                                .hasAuthority(String.valueOf(Role.TRAINER))
                                 .anyRequest()
                                 .authenticated()
                 ).userDetailsService(userDetailsImplService)
