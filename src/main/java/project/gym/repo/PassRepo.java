a9d786ed5a2e25bafd8a389bfca3cdb6f9931587
Blindeen <kuba.szmajda@gmail.com>
2024-06-05 09:21:26 +0200

    handle pass type and payment method type in register endpoint

--- src/main/java/project/gym/repo/PassRepo.java
+++ src/main/java/project/gym/repo/PassRepo.java
@@ -1,7 +1,9 @@
 package project.gym.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import project.gym.enums.PassType;
 import project.gym.model.Pass;
 
 public interface PassRepo extends CrudRepository<Pass, Long> {
+    Pass findPassByType(PassType type);
 }
--- src/main/java/project/gym/repo/PaymentMethodRepo.java
+++ src/main/java/project/gym/repo/PaymentMethodRepo.java
@@ -1,7 +1,9 @@
 package project.gym.repo;
 
 import org.springframework.data.repository.CrudRepository;
+import project.gym.enums.PaymentMethodType;
 import project.gym.model.PaymentMethod;
 
 public interface PaymentMethodRepo extends CrudRepository<PaymentMethod, Long> {
+    PaymentMethod findPaymentMethodByType(PaymentMethodType type);
 }
