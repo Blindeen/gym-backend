494c18999c316f14c279595c38b6a06e09ec3e5e
Blindeen <kuba.szmajda@gmail.com>
2024-07-14 20:48:39 +0200

    add first name and last name to auth response

--- src/main/java/project/gym/dto/member/MemberInfo.java
+++ src/main/java/project/gym/dto/member/MemberInfo.java
@@ -5,12 +5,15 @@ import project.gym.model.Member;
 
 @Data
 public class MemberInfo {
+    private String firstName;
+    private String lastName;
     private String email;
-
     private String role;
 
     public static MemberInfo valueOf(Member member) {
         MemberInfo memberInfo = new MemberInfo();
+        memberInfo.firstName = member.getFirstName();
+        memberInfo.lastName = member.getLastName();
         memberInfo.email = member.getEmail();
         memberInfo.role = member.getRole().toString();
 
