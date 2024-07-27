b3f64ef210c61049d6bdcf42845b646aae060734
Blindeen <kuba.szmajda@gmail.com>
2024-07-27 17:07:28 +0200

    configure simple email sender

---
+++ src/main/java/project/gym/service/MailService.java
@@ -0,0 +1,22 @@
+package project.gym.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender emailSender;
+
+    public void sendSimpleMessage(String to, String subject, String text) throws MailException {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("fitsphere.info@gmail.com");
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
