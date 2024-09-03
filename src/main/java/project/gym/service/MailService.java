68f0c7fa7d06183fb8c043143f6c7c4ccbef5b07
Blindeen <kuba.szmajda@gmail.com>
2024-09-03 16:43:53 +0200

    translate sign-up confirmation email subject

--- src/main/java/project/gym/service/MailService.java
+++ src/main/java/project/gym/service/MailService.java
@@ -3,6 +3,7 @@ package project.gym.service;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -20,18 +21,19 @@ public class MailService {
 
     private final JavaMailSender emailSender;
     private final TemplateEngine htmlTemplateEngine;
+    private final MessageSource messageSource;
 
-    public MailService(JavaMailSender emailSender, TemplateEngine htmlTemplateEngine) {
+    public MailService(JavaMailSender emailSender, TemplateEngine htmlTemplateEngine, MessageSource messageSource) {
         this.emailSender = emailSender;
         this.htmlTemplateEngine = htmlTemplateEngine;
+        this.messageSource = messageSource;
     }
 
     private void sendMail(
             String to,
             String subject,
             String templateName,
-            Map<String, Object> vars
-    ) throws MessagingException {
+            Map<String, Object> vars) throws MessagingException {
         MimeMessage mimeMessage = emailSender.createMimeMessage();
         MimeMessageHelper email = new MimeMessageHelper(mimeMessage, true, "UTF-8");
         email.setFrom(username);
@@ -51,11 +53,12 @@ public class MailService {
     public void sendSignUpConfirmation(String emailTo, String firstName) {
         Map<String, Object> vars = Map.of(
                 "email", emailTo,
-                "firstName", firstName
-        );
+                "firstName", firstName);
+        String subject = messageSource.getMessage("email.signUpConfirmation", null,
+                LocaleContextHolder.getLocale());
 
         try {
-            sendMail(emailTo, "Sign-up confirmation", "signup", vars);
+            sendMail(emailTo, subject, "signup", vars);
         } catch (MessagingException e) {
             System.err.println("Failed to send email: " + e.getMessage());
         }
