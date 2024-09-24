6dc2213b0dc4f2927f2d0fbaa0e00f886eea5902
Blindeen <kuba.szmajda@gmail.com>
2024-09-24 15:00:20 +0200

    remove unused imports

--- src/main/java/project/gym/dto/member/UpdateMemberRequest.java
+++ src/main/java/project/gym/dto/member/UpdateMemberRequest.java
@@ -1,6 +1,5 @@
 package project.gym.dto.member;
 
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
