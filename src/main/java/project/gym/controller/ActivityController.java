ee2d76bd5c8ff3404f929ca7e3a9cb59df08ca8a
Blindeen <kuba.szmajda@gmail.com>
2024-10-27 02:41:57 +0200

    organize activity controller

--- src/main/java/project/gym/controller/ActivityController.java
+++ src/main/java/project/gym/controller/ActivityController.java
@@ -1,13 +1,22 @@
 package project.gym.controller;
 
-import jakarta.validation.Valid;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import project.gym.dto.activity.ActivityResponse;
 import project.gym.dto.activity.CreateActivityRequest;
 import project.gym.dto.activity.UpdateActivityRequest;
@@ -16,7 +25,7 @@ import project.gym.service.ActivityService;
 import project.gym.service.JwtService;
 
 @RestController
-@RequestMapping("/activity")
+@RequestMapping("/activities")
 public class ActivityController {
     private final ActivityService activityService;
     private final JwtService jwtService;
@@ -26,7 +35,16 @@ public class ActivityController {
         this.jwtService = jwtService;
     }
 
-    @PostMapping("/create")
+    @GetMapping("")
+    public ResponseEntity<Page<ActivityResponse>> get(
+            @RequestParam(defaultValue = "1") int pageNumber,
+            @RequestParam(defaultValue = "5") int pageSize) {
+        Pageable pagination = PageRequest.of(pageNumber - 1, pageSize);
+        Page<ActivityResponse> activities = activityService.getActivities(pagination);
+        return new ResponseEntity<>(activities, HttpStatus.OK);
+    }
+
+    @PostMapping("")
     public ResponseEntity<ActivityResponse> create(
             @RequestHeader("Authorization") String token,
             @RequestBody @Valid CreateActivityRequest requestBody) {
@@ -35,16 +53,8 @@ public class ActivityController {
         return new ResponseEntity<>(newActivity, HttpStatus.CREATED);
     }
 
-    @GetMapping("/list")
-    public ResponseEntity<Page<ActivityResponse>> list(
-            @RequestParam(defaultValue = "1") int pageNumber,
-            @RequestParam(defaultValue = "5") int pageSize) {
-        Pageable pagination = PageRequest.of(pageNumber - 1, pageSize);
-        Page<ActivityResponse> activities = activityService.getActivities(pagination);
-        return new ResponseEntity<>(activities, HttpStatus.OK);
-    }
 
-    @PutMapping("/{id}/update")
+    @PutMapping("/{id}")
     public ResponseEntity<ActivityResponse> update(
             @PathVariable Long id,
             @RequestBody @Valid UpdateActivityRequest requestBody) {
@@ -52,13 +62,13 @@ public class ActivityController {
         return new ResponseEntity<>(activity, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}/delete")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         activityService.deleteActivity(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
-    @PostMapping("/{id}/enroll")
+    @PostMapping("/{id}/enrollment")
     public ResponseEntity<Void> enroll(
             @PathVariable Long id,
             @RequestHeader("Authorization") String token) {
