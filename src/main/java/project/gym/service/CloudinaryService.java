cea3c5acc0363b27ed4bc308ffa48ecb2e53ab05
Blindeen <kuba.szmajda@gmail.com>
2024-10-28 22:40:56 +0100

    export map property names to static attributes

--- src/main/java/project/gym/service/CloudinaryService.java
+++ src/main/java/project/gym/service/CloudinaryService.java
@@ -12,6 +12,15 @@ import com.cloudinary.utils.ObjectUtils;
 
 @Service
 public class CloudinaryService {
+    private final static String CLOUD_NAME_KEY = "cloud_name";
+    private final static String API_KEY_KEY = "api_key";
+    private final static String API_SECRET_KEY = "api_secret";
+    private final static String SECURE_KEY = "secure";
+
+    private final static String PUBLIC_ID_KEY = "public_id";
+    private final static String RESOURCE_TYPE_KEY = "resource_type";
+    private final static String OVERWRITE_KEY = "overwrite";
+
     private final Cloudinary cloudinary;
 
     public CloudinaryService(
@@ -19,20 +28,19 @@ public class CloudinaryService {
             @Value("${cloudinary.api.key}") String apiKey,
             @Value("${cloudinary.api.secret}") String apiSecret) {
         cloudinary = new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret,
-                "secure", true));
+                CLOUD_NAME_KEY, cloudName,
+                API_KEY_KEY, apiKey,
+                API_SECRET_KEY, apiSecret,
+                SECURE_KEY, true));
     }
 
     public String uploadImage(MultipartFile multipartFile, String publicID) throws IOException {
         Map<String, Object> params = ObjectUtils.asMap(
-                "public_id", publicID,
-                "resource_type", "image",
-                "overwrite", true
-        );
+                PUBLIC_ID_KEY, publicID,
+                RESOURCE_TYPE_KEY, "image",
+                OVERWRITE_KEY, true);
         Map<String, Object> response = cloudinary.uploader().upload(multipartFile.getBytes(), params);
-        return (String) response.get("url");
+        return (String) response.get("secure_url");
     }
 
 }
